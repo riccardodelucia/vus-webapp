6742ad6d4a47f1d6954a7d175fab09c7df85eec5
Ananya Hans <ananya.hans@fht.org>
2023-12-04 16:38:09 +0100

    code-review pt.2

--- src/views/ViewHome.vue
+++ src/views/ViewHome.vue
@@ -43,78 +43,75 @@
           </p>
         </section>
 
-        <div class="section-wrapper">
-          <section class="section-grid section-grid-1">
-            <div class="ht-layout-stack">
-              <h2>Step 1: Start by typing in a gene or selecting a variant</h2>
-              <ul>
-                <li>Inputting the targeted <b>gene</b></li>
-                <li>Selecting a <b>variant</b> from the chart</li>
-              </ul>
-              <p>
-                DAMs found in at least one patient of the corresponding cancer
-                type where they were identified as dependency associated. Labels
-                indicating gene name and aminoacidic variation are coloured
-                according to previous indication of a gene being a driver.
-              </p>
-              <p>
-                Orange bars represent the number of patients of the
-                corresponding cancer type carrying the specific DAM: light and
-                dark orange indicate different clinical databases.
-              </p>
-              <p>
-                Purple bars represent the number of cancer types where the DAM
-                was both identified as dependency-associated (according to our
-                pipeline) and found in at least one patient. Red dots and
-                triangles label variants predicted as strongly impacting the
-                gene function according to two algorithms (SIFT or PolyPhen).
-              </p>
-            </div>
-            <img
-              src="@/assets/img/section-1.png"
-              alt="Image of the first page of the application showing the search bar and the interactive circle chart"
-            />
-          </section>
-          <section class="section-grid section-grid-2">
-            <div class="ht-layout-stack">
-              <h2>Step 2: Click on any squares of the heat-map</h2>
-              <p>
-                Summary of variants for a given gene with DAM characterization,
-                deleterious effect prediction, representation across cancer
-                types and frequency in patient carrying the alteration, with the
-                possibility for direct insight on its essentiality across the
-                underlying cell lines.
-              </p>
-            </div>
-            <img
-              src="@/assets/img/section-2.png"
-              alt="Image showing a heat map of patients data and gene variants"
-            />
-          </section>
-          <section class="section-grid section-grid-1">
-            <div class="ht-layout-stack">
-              <h2>Step 3: LFC graph</h2>
-              <ul>
-                <li>Use drop down to access other <b>drugs</b></li>
-                <li>
-                  Selecting between <b>essentiallity</b> or <b>sensitivity</b>
-                </li>
-              </ul>
-              <p>
-                Projection of the sorted fitness of all cell lines within a
-                given cancer type known for their presence and absence of a
-                studied VUS, obtained by essentiality assessment using CRISPR
-                and pharmacological inhibition by drugs targeting this
-                variation.
-              </p>
-            </div>
+        <section class="section-grid">
+          <div class="ht-layout-stack">
+            <h2>Step 1: Start by typing in a gene or selecting a variant</h2>
+            <ul>
+              <li>Inputting the targeted <b>gene</b></li>
+              <li>Selecting a <b>variant</b> from the chart</li>
+            </ul>
+            <p>
+              DAMs found in at least one patient of the corresponding cancer
+              type where they were identified as dependency associated. Labels
+              indicating gene name and aminoacidic variation are coloured
+              according to previous indication of a gene being a driver.
+            </p>
+            <p>
+              Orange bars represent the number of patients of the corresponding
+              cancer type carrying the specific DAM: light and dark orange
+              indicate different clinical databases.
+            </p>
+            <p>
+              Purple bars represent the number of cancer types where the DAM was
+              both identified as dependency-associated (according to our
+              pipeline) and found in at least one patient. Red dots and
+              triangles label variants predicted as strongly impacting the gene
+              function according to two algorithms (SIFT or PolyPhen).
+            </p>
+          </div>
+          <img
+            src="@/assets/img/section-1.png"
+            alt="Image of the first page of the application showing the search bar and the interactive circle chart"
+          />
+        </section>
+        <section class="section-grid section-grid-alt">
+          <div class="ht-layout-stack">
+            <h2>Step 2: Click on any squares of the heat-map</h2>
+            <p>
+              Summary of variants for a given gene with DAM characterization,
+              deleterious effect prediction, representation across cancer types
+              and frequency in patient carrying the alteration, with the
+              possibility for direct insight on its essentiality across the
+              underlying cell lines.
+            </p>
+          </div>
+          <img
+            src="@/assets/img/section-2.png"
+            alt="Image showing a heat map of patients data and gene variants"
+          />
+        </section>
+        <section class="section-grid">
+          <div class="ht-layout-stack">
+            <h2>Step 3: LFC graph</h2>
+            <ul>
+              <li>Use drop down to access other <b>drugs</b></li>
+              <li>
+                Selecting between <b>essentiallity</b> or <b>sensitivity</b>
+              </li>
+            </ul>
+            <p>
+              Projection of the sorted fitness of all cell lines within a given
+              cancer type known for their presence and absence of a studied VUS,
+              obtained by essentiality assessment using CRISPR and
+              pharmacological inhibition by drugs targeting this variation.
+            </p>
+          </div>
 
-            <img
-              src="@/assets/img/section-3.png"
-              alt="Image showing sensitivity and essentiality charts on cell lines"
-            />
-          </section>
-        </div>
+          <img
+            src="@/assets/img/section-3.png"
+            alt="Image showing sensitivity and essentiality charts on cell lines"
+          />
+        </section>
       </div>
     </main>
     <ht-footer>
@@ -142,91 +139,66 @@ export default {
 
   img {
     max-width: 40rem;
-  }
-
-  @media (max-width: 55em) {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-
-    img {
-      width: 100%;
-    }
+    width: 100%;
   }
 }
 
 .intro {
   padding: var(--size-6);
-  border-top: 1px solid lightgray;
-  border-bottom: 1px solid lightgray;
+  border-top: 1px solid var(--ht-color-gray-1);
+  border-bottom: 1px solid var(--ht-color-gray-1);
+
+  /* p {
+    max-width: 70ch;
+    margin: auto;
+  } */
 }
 
 section {
   margin-bottom: var(--size-10);
 }
 
-.section-wrapper {
-  width: min(var(--width, 80rem), 100% - 2em);
-  margin-inline: auto;
-  margin-top: 10%;
-}
-
 .section-grid {
   display: grid;
 
   grid-template-columns: repeat(2, 1fr);
-  grid-template-rows: 1fr;
-  grid-column-gap: 10px;
-  grid-row-gap: 20px;
-
-  gap: var(--size-8);
+  gap: var(--size-10);
 
   place-items: center;
 
+  grid-template-areas: 'image info';
+
   img {
     box-shadow: var(--shadow-3);
-    grid-area: image;
+    width: 100%;
+    max-width: 40rem;
   }
 
-  p,
-  ul {
-    max-width: 70ch;
+  & > *:first-child {
     grid-area: info;
   }
 
-  &.section-grid-1 {
-    grid-template-columns: 1fr 1fr;
-    grid-template-areas: 'image info';
-
-    @media (max-width: 55em) {
-      box-shadow: var(--shadow-3);
-      padding: 2rem;
-
-      grid-template-columns: 1fr;
-      grid-template-rows: 1fr;
-
-      grid-template-areas:
-        'info'
-        'image';
-    }
+  & > *:last-child {
+    grid-area: image;
   }
 
-  &.section-grid-2 {
-    grid-template-columns: 1fr 1fr;
-    grid-template-areas: 'info image';
+  @media (max-width: 65em) {
+    grid-template-columns: 1fr;
 
-    @media (max-width: 55em) {
-      box-shadow: var(--shadow-3);
-      padding: 2rem;
+    grid-template-areas:
+      'info'
+      'image';
+  }
+}
 
-      grid-template-columns: 1fr;
-      grid-template-rows: 1fr;
+.section-grid-alt {
+  grid-template-areas: 'info image';
+  @media (max-width: 65em) {
+    grid-template-columns: 1fr;
 
-      grid-template-areas:
-        'info'
-        'image';
-    }
+    grid-template-areas:
+      'info'
+      'image';
   }
 }
 
